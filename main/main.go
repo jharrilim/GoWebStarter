cd7cee953782c689bbd7993b8edb93e7e7113b5c
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2018-05-01 17:53:56 -0400

    simple

--- main/main.go
+++ main/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func main() {
-	http.ListenAndServe(":8080", http.FileServer(http.Dir("public")))
-	router := mux.NewRouter()
-	router.HandleFunc("/Topics", GetTopics).Methods("GET")
-	
+	http.ListenAndServe(":80", http.FileServer(http.Dir("public")))
+
 }
