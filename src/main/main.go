bae1bce90db9a1eb2f679a6632c587dedd38a1bd
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2018-04-30 20:34:25 -0400

    read files through http

--- src/main/main.go
+++ src/main/main.go
@@ -1,11 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"io/ioutil"
+)
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request){
-		w.Write([]byte("Hello World"))
-	})
-
+	http.Handle("/", new(MyHandler))
 	http.ListenAndServe(":8080", nil)
+}
+
+type MyHandler struct {
+	http.Handler
+}
+
+func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	path := "public/" + req.URL.Path
+	data, err := ioutil.ReadFile(string(path))
+
+	if err == nil {
+		w.Write(data)
+	} else {
+		w.WriteHeader(404)
+		w.Write([]byte("404 - " + http.StatusText(404)))
+	}
 }
