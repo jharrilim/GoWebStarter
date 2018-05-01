361fa6f3b8ac5ae6f85467899a6257f933d8f317
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2018-04-30 20:42:58 -0400

    write content type header

--- src/main/main.go
+++ src/main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"io/ioutil"
+	"strings"
 )
 
 func main() {
@@ -19,6 +20,19 @@ func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	data, err := ioutil.ReadFile(string(path))
 
 	if err == nil {
+		var contentType string
+		if strings.HasSuffix(path, ".css") {
+			contentType = "text/css"
+		} else if strings.HasSuffix(path, ".html") {
+			contentType = "text/html"
+		} else if strings.HasSuffix(path, ".js") {
+			contentType = "application/javascript"
+		} else if strings.HasSuffix(path, ".png") {
+			contentType = "image/png"
+		} else {
+			contentType = "text/plain"
+		}
+		w.Header().Add("Content-Type", contentType)
 		w.Write(data)
 	} else {
 		w.WriteHeader(404)
