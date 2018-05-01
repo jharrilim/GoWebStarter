f46aaa4f1e23c055429a1df18b371461a38e00e7
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2018-04-30 20:50:08 -0400

    buffered out, handle mp4

--- src/main/main.go
+++ src/main/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"net/http"
-	"io/ioutil"
 	"strings"
+	"os"
+	"bufio"
 )
 
 func main() {
@@ -17,9 +18,10 @@ type MyHandler struct {
 
 func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	path := "public/" + req.URL.Path
-	data, err := ioutil.ReadFile(string(path))
+	f, err := os.Open(path)
 
 	if err == nil {
+		bufferedReader := bufio.NewReader(f)
 		var contentType string
 		if strings.HasSuffix(path, ".css") {
 			contentType = "text/css"
@@ -29,11 +31,13 @@ func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 			contentType = "application/javascript"
 		} else if strings.HasSuffix(path, ".png") {
 			contentType = "image/png"
+		} else if strings.HasSuffix(path, ".mp4") {
+			contentType ="video/mp4"
 		} else {
 			contentType = "text/plain"
 		}
 		w.Header().Add("Content-Type", contentType)
-		w.Write(data)
+		bufferedReader.WriteTo(w)
 	} else {
 		w.WriteHeader(404)
 		w.Write([]byte("404 - " + http.StatusText(404)))
