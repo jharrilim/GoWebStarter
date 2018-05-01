f75054eb96f84a4807defc12a3167675432dd324
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2018-05-01 17:31:38 -0400

    refactor

--- main/main.go
+++ main/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func main() {
 	http.ListenAndServe(":8080", http.FileServer(http.Dir("public")))
+	router := mux.NewRouter()
+	router.HandleFunc("/Topics", GetTopics).Methods("GET")
+	
 }
