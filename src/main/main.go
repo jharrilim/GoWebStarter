9eaab30330bb53229363f1bc0ff4d3f9f5b713d9
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2018-04-30 20:53:41 -0400

    refactor, RIP

--- src/main/main.go
+++ src/main/main.go
@@ -2,44 +2,8 @@ package main
 
 import (
 	"net/http"
-	"strings"
-	"os"
-	"bufio"
 )
 
 func main() {
-	http.Handle("/", new(MyHandler))
-	http.ListenAndServe(":8080", nil)
-}
-
-type MyHandler struct {
-	http.Handler
-}
-
-func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	path := "public/" + req.URL.Path
-	f, err := os.Open(path)
-
-	if err == nil {
-		bufferedReader := bufio.NewReader(f)
-		var contentType string
-		if strings.HasSuffix(path, ".css") {
-			contentType = "text/css"
-		} else if strings.HasSuffix(path, ".html") {
-			contentType = "text/html"
-		} else if strings.HasSuffix(path, ".js") {
-			contentType = "application/javascript"
-		} else if strings.HasSuffix(path, ".png") {
-			contentType = "image/png"
-		} else if strings.HasSuffix(path, ".mp4") {
-			contentType ="video/mp4"
-		} else {
-			contentType = "text/plain"
-		}
-		w.Header().Add("Content-Type", contentType)
-		bufferedReader.WriteTo(w)
-	} else {
-		w.WriteHeader(404)
-		w.Write([]byte("404 - " + http.StatusText(404)))
-	}
+	http.ListenAndServe(":8080", http.FileServer("public"))
 }
